2145a56c22fb2f9c8455014eb88fe74e4c03cd42
mehmetkarakus <113180215+mehmetkarakus@users.noreply.github.com>
2023-11-10 11:00:49 +0300

    update

--- main.js
+++ main.js
@@ -5,83 +5,83 @@ import './component/card-capital.js'
 
 document.querySelector('#app').innerHTML = `
 
-  <section class="search">
-    <input type="text" placeholder="Şehir İsmi Giriniz">
-    <button>
-      <img src="img/search.png">
-    </button>
-  </section>
+    <section class="search">
+      <input type="text" placeholder="Şehir İsmi Giriniz">
+      <button>
+        <img src="img/search.png">
+      </button>
+    </section>
 
-  <section id="card__container" class="card__capital">
+    <section id="card__container" class="card__capital">
+        
+    </section>
+
+    <section id="card__city" class="card__city">
+
+        <div class="weather">
+          <div class="icon">
+            <img src="img/rain.png" class="weather__icon">
+          </div>
+          <div class="details__city">
+            <h1 class="temp">22 °C</h1>
+            <h2 class="city">Manisa</h2>
+          </div>
+        </div>
+
+        <div class="details">
+          <div class="details__information">
+            <div class="col">
+              <img src="img/humidity.png">
+              <div>
+                <p class="humidity">50%</p>
+                <p>Humidity</p>
+              </div>
+            </div>
+
+            <div class="col">
+              <img src="img/wind.png">
+              <div>
+                <p class="wind">2 km/h</p>
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    </section> 
+
+    <section id="daily__weather" class="daily__weather">
       
-  </section>
-
-  <section id="card__city" class="card__city">
-
-      <div class="weather">
-        <div class="icon">
-          <img src="img/rain.png" class="weather__icon">
-        </div>
-        <div class="details__city">
-          <h1 class="temp">22 °C</h1>
-          <h2 class="city">Manisa</h2>
-        </div>
+      <div class="card">
+        <h3>Wen</h3>
+        <img src="img/rain.png" class="card__icon">
+        <h5>14 °C</h5>
       </div>
 
-      <div class="details">
-        <div class="details__information">
-          <div class="col">
-            <img src="img/humidity.png">
-            <div>
-              <p class="humidity">50%</p>
-              <p>Humidity</p>
-            </div>
-          </div>
-
-          <div class="col">
-            <img src="img/wind.png">
-            <div>
-              <p class="wind">2 km/h</p>
-              <p>Wind Speed</p>
-            </div>
-          </div>
-        </div>
+      <div class="card">
+        <h3>Wen</h3>
+        <img src="img/rain.png" class="card__icon">
+        <h5>14 °C</h5>
       </div>
 
-  </section> 
+      <div class="card">
+        <h3>Wen</h3>
+        <img src="img/rain.png" class="card__icon">
+        <h5>14 °C</h5>
+      </div>
 
-  <section id="daily__weather" class="daily__weather">
-    
-    <div class="card">
-      <h3>Wen</h3>
-      <img src="img/rain.png" class="card__icon">
-      <h5>14 °C</h5>
-    </div>
+      <div class="card">
+        <h3>Wen</h3>
+        <img src="img/rain.png" class="card__icon">
+        <h5>14 °C</h5>
+      </div>
 
-    <div class="card">
-      <h3>Wen</h3>
-      <img src="img/rain.png" class="card__icon">
-      <h5>14 °C</h5>
-    </div>
+      <div class="card">
+        <h3>Wen</h3>
+        <img src="img/rain.png" class="card__icon">
+        <h5>14 °C</h5>
+      </div>
 
-    <div class="card">
-      <h3>Wen</h3>
-      <img src="img/rain.png" class="card__icon">
-      <h5>14 °C</h5>
-    </div>
-
-    <div class="card">
-      <h3>Wen</h3>
-      <img src="img/rain.png" class="card__icon">
-      <h5>14 °C</h5>
-    </div>
-
-    <div class="card">
-      <h3>Wen</h3>
-      <img src="img/rain.png" class="card__icon">
-      <h5>14 °C</h5>
-    </div>
-
-  </section>
+    </section>
     
 `
