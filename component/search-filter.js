1fa34c5c47cfd2bb2594f8960c12be8051eae38f
mehmetkarakus <113180215+mehmetkarakus@users.noreply.github.com>
2023-11-28 16:32:36 +0300

    update

--- component/search-filter.js
+++ component/search-filter.js
@@ -18,18 +18,93 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   
     function displayResults(searchTerm) {
-       var items = ["Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Amasya", "Ankara", "Antalya", "Artvin","Burdur","Bolu","Bitlis","Bingöl","Bilecik","Balıkesir","Aydın",
-        "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Elazığ", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Isparta", 
-        "Mersin", "İstanbul", "İzmir", "Kars", "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Kahramanmaraş", "Mardin", 
-        "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Uşak", 
-        "Van", "Yozgat", "Zonguldak", "Aksaray", "Bayburt", "Karaman", "Kırıkkale", "Batman", "Şırnak", "Bartın", "Ardahan", "Iğdır", "Yalova", "Karabük", "Kilis", "Osmaniye", 
-        "Düzce"];
-
-        var filteredItems = items.filter(function (item) {
-            return item.toLowerCase().includes(searchTerm);
-        });
-
-        renderResults(filteredItems);
+      var items = [
+        "Adana",
+        "Adıyaman",
+        "Afyonkarahisar",
+        "Ağrı",
+        "Amasya",
+        "Ankara",
+        "Antalya",
+        "Artvin",
+        "Burdur",
+        "Bolu",
+        "Bitlis",
+        "Bingöl",
+        "Bilecik",
+        "Balıkesir",
+        "Aydın",
+        "Bursa",
+        "Çanakkale",
+        "Çankırı",
+        "Çorum",
+        "Denizli",
+        "Diyarbakır",
+        "Elazığ",
+        "Erzurum",
+        "Eskişehir",
+        "Gaziantep",
+        "Giresun",
+        "Gümüşhane",
+        "Hakkari",
+        "Hatay",
+        "Isparta",
+        "Mersin",
+        "İstanbul",
+        "İzmir",
+        "Kars",
+        "Kastamonu",
+        "Kayseri",
+        "Kırklareli",
+        "Kırşehir",
+        "Kocaeli",
+        "Konya",
+        "Kütahya",
+        "Malatya",
+        "Manisa",
+        "Kahramanmaraş",
+        "Mardin",
+        "Muğla",
+        "Muş",
+        "Nevşehir",
+        "Niğde",
+        "Ordu",
+        "Rize",
+        "Sakarya",
+        "Samsun",
+        "Siirt",
+        "Sinop",
+        "Sivas",
+        "Tekirdağ",
+        "Tokat",
+        "Trabzon",
+        "Tunceli",
+        "Şanlıurfa",
+        "Uşak",
+        "Van",
+        "Yozgat",
+        "Zonguldak",
+        "Aksaray",
+        "Bayburt",
+        "Karaman",
+        "Kırıkkale",
+        "Batman",
+        "Şırnak",
+        "Bartın",
+        "Ardahan",
+        "Iğdır",
+        "Yalova",
+        "Karabük",
+        "Kilis",
+        "Osmaniye",
+        "Düzce",
+      ];
+  
+      var filteredItems = items.filter(function (item) {
+        return item.toLocaleLowerCase("TR").includes(searchTerm);
+      });
+  
+      renderResults(filteredItems);
     }
   
     function renderResults(results) {
