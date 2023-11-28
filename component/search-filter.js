7f9d1f14d2dd35f1784197d6221829ba9d9288c3
Mehmet Karakuş <mehmetkrks438@gmail.com>
2023-11-28 21:00:40 +0300

    update

--- component/search-filter.js
+++ component/search-filter.js
@@ -1,132 +1,52 @@
-document.addEventListener("DOMContentLoaded", () => {
-    var searchInput = document.getElementById("searchInput");
-  
-    var searchResults = document.getElementById("searchResults");
-  
-    searchInput.addEventListener("input", function () {
-      performSearch();
-    });
-  
-    function performSearch() {
-      var searchTerm = searchInput.value.toLocaleLowerCase("TR");
-  
-      if (searchTerm === "") {
-        searchResults.style.display = "none";
-        return;
-      }
-      displayResults(searchTerm);
-    }
-  
-    function displayResults(searchTerm) {
-      var items = [
-        "Adana",
-        "Adıyaman",
-        "Afyonkarahisar",
-        "Ağrı",
-        "Amasya",
-        "Ankara",
-        "Antalya",
-        "Artvin",
-        "Burdur",
-        "Bolu",
-        "Bitlis",
-        "Bingöl",
-        "Bilecik",
-        "Balıkesir",
-        "Aydın",
-        "Bursa",
-        "Çanakkale",
-        "Çankırı",
-        "Çorum",
-        "Denizli",
-        "Diyarbakır",
-        "Elazığ",
-        "Erzurum",
-        "Eskişehir",
-        "Gaziantep",
-        "Giresun",
-        "Gümüşhane",
-        "Hakkari",
-        "Hatay",
-        "Isparta",
-        "Mersin",
-        "İstanbul",
-        "İzmir",
-        "Kars",
-        "Kastamonu",
-        "Kayseri",
-        "Kırklareli",
-        "Kırşehir",
-        "Kocaeli",
-        "Konya",
-        "Kütahya",
-        "Malatya",
-        "Manisa",
-        "Kahramanmaraş",
-        "Mardin",
-        "Muğla",
-        "Muş",
-        "Nevşehir",
-        "Niğde",
-        "Ordu",
-        "Rize",
-        "Sakarya",
-        "Samsun",
-        "Siirt",
-        "Sinop",
-        "Sivas",
-        "Tekirdağ",
-        "Tokat",
-        "Trabzon",
-        "Tunceli",
-        "Şanlıurfa",
-        "Uşak",
-        "Van",
-        "Yozgat",
-        "Zonguldak",
-        "Aksaray",
-        "Bayburt",
-        "Karaman",
-        "Kırıkkale",
-        "Batman",
-        "Şırnak",
-        "Bartın",
-        "Ardahan",
-        "Iğdır",
-        "Yalova",
-        "Karabük",
-        "Kilis",
-        "Osmaniye",
-        "Düzce",
-      ];
-  
-      var filteredItems = items.filter(function (item) {
-        return item.toLocaleLowerCase("TR").includes(searchTerm);
-      });
-  
-      renderResults(filteredItems);
-    }
-  
-    function renderResults(results) {
-      searchResults.innerHTML = "";
-  
-      results.forEach(function (result) {
-        var li = document.createElement("li");
-        li.textContent = result;
-  
-        li.addEventListener("mouseover", function () {
-          searchInput.value = result;
-        });
-  
-        li.addEventListener("click", function () {
-          searchResults.style.display = "none";
-        });
-  
-        searchResults.appendChild(li);
-      });
-  
-      searchResults.style.display = "block";
-    }
-  
+document.addEventListener("DOMContentLoaded", async () => {
+  var searchInput = document.getElementById("searchInput");
+  var searchResults = document.getElementById("searchResults");
+
+  searchInput.addEventListener("input", function () {
+    performSearch();
   });
-  
+
+  async function performSearch() {
+    var searchTerm = searchInput.value.toLocaleLowerCase("TR");
+
+    if (searchTerm === "") {
+      searchResults.style.display = "none";
+      return;
+    }
+
+    const response = await fetch("./component/city.json");
+    const items = await response.json();
+
+    displayResults(searchTerm, items);
+  }
+
+  function displayResults(searchTerm, items) {
+    var filteredItems = items.filter(function (item) {
+      return item.toLocaleLowerCase("tr-TR").includes(searchTerm);
+    });
+    
+
+    renderResults(filteredItems);
+  }
+
+  function renderResults(results) {
+    searchResults.innerHTML = "";
+
+    results.forEach(function (result) {
+      var li = document.createElement("li");
+      li.textContent = result;
+
+      li.addEventListener("mouseover", function () {
+        searchInput.value = result;
+      });
+
+      li.addEventListener("click", function () {
+        searchResults.style.display = "none";
+      });
+
+      searchResults.appendChild(li);
+    });
+
+    searchResults.style.display = "block";
+  }
+});
